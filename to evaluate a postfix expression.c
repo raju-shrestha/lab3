bd0357cff82a61bf91f82c6985295d6b77fe11d1
Raju shrestha <raju.shrestha@deerwalk.edu.np>
2016-07-17 15:43:29 +0545

    Add files via upload

---
+++ to evaluate a postfix expression.c
@@ -0,0 +1,93 @@
+//To evaluate a postfix expression
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAXSIZE 50
+
+typedef struct {
+	int top;
+	int items[MAXSIZE];
+} stack ;
+
+void push(stack *, int);
+double pop(stack *);
+
+int evaluate(char[]);
+int isdigit(int);
+int operation(int, int, int);
+
+main(){
+	char exp[50];
+	
+	printf ("Enter an expression \n");
+	gets(exp);
+	
+	printf ("%s \n", exp);
+	
+	printf ("Result = %d", evaluate(exp));
+	
+	getch();
+}
+
+void push(stack *s, int x) {
+	if (s->top == (MAXSIZE-1)){
+		printf ("Stack Overflow \n");
+		getch();
+		exit(1);	
+	} else {
+		s->items[++(s->top)] = x;
+	}
+}
+
+double pop(stack *s){
+	if (s->top == -1){
+		printf ("Stack Underflow \n");
+		getch();
+		exit (1);
+	} else {
+		return(s->items[s->top--]);
+	}
+}
+
+int evaluate(char exp[]) {
+	
+	stack s;
+	s.top = -1;
+	int symb, operand1, operand2, value;
+	int i = 0;
+	
+	while (exp[i] != '\0'){
+		
+		symb = exp[i];
+		
+		if (isdigit(symb)) {
+			push (&s, (int) (symb-'0'));
+		} else {
+			operand2 = pop(&s);
+			operand1 = pop(&s);
+			
+			value = operation(symb, operand1, operand2);
+			
+			push (&s, value);
+		}
+		i++;
+	}
+	return (pop(&s));
+}
+
+int isdigit(int symb) {
+	return (symb >= '0' && symb <= '9');
+}
+
+int operation(int symb, int operand1, int operand2) {
+	switch (symb) {
+		case '+'	: return (operand1 + operand2);
+		case '-'	: return (operand1 - operand2);
+		case '*'	: return (operand1 * operand2);
+		case '/'	: return (operand1 / operand2);
+		case '$'	: return (pow(operand1, operand2));
+		default 	: printf ("Invalid Operation! \n");
+	}
+}
---
+++ to find if a mathematical expression is valid or not.c
@@ -0,0 +1,97 @@
+//To find if a mathematical expression is valid or not
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 100
+
+typedef struct {
+	int top;
+	char items[MAXSIZE];
+} stack;
+
+void push (stack *s, char);
+char pop (stack *s);
+void valid_not (char exp[]);
+int ismatching_opener(char);
+
+main ()
+{
+	char exp[100];
+	
+	printf ("Enter an expression \n");
+	gets(exp);
+	
+	printf ("%s \n", exp);
+	
+	valid_not(exp);
+	
+	getch();
+}
+
+void push(stack *s, char x) {
+	if (s->top == (MAXSIZE-1)){
+		printf ("Stack Overflow \n");
+		getch();
+		exit(1);	
+	} else {
+		s->items[++(s->top)] = x;
+	}
+}
+
+char pop(stack *s){
+	if (s->top == -1){
+		printf ("Stack Underflow \n");
+		getch();
+		exit (1);
+	} else {
+		return(s->items[s->top--]);
+	}
+}
+
+void valid_not(char exp[]) {
+	char symb, j;
+	int valid = 1;
+	
+	stack s;
+	s.top = -1;
+	int i = 0;
+	
+	while (exp[i] != '\0'){
+		
+		symb = exp[i];
+		
+		if (symb == '(' || symb == '[' || symb == '{') {
+			push(&s, symb);
+		} 
+		if (symb == ')' || symb == ']' || symb == '}') {
+			if (s.top == -1) {
+				valid = 0;
+			} else {
+				if (ismatching_opener(symb)) {
+					valid = 0;
+				}
+			}
+		}
+		i++;
+	}
+	
+	if (s.top != -1) {
+		valid = 0;
+	} 
+	
+	if (valid == 1) {
+		printf ("The string is valid \n");
+	} else {
+		printf ("The string is not valid \n");
+	}
+}
+
+int ismatching_opener(char symb) {
+	
+	stack s;
+	int i;
+	i = pop(&s);
+	
+	return ((symb == ')' && i != '(') || (symb == '}' && i != '{') || (symb == ']' && i != '['));
+}
